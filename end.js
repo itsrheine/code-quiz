78fbffd5a5b5bb611903ca569f965c370a075c09
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-15 15:29:09 -0700

    save highscores in local storage

--- end.js
+++ end.js
@@ -1,14 +1,19 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+// lcan still save high scores by converting into JSON
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
 
 // this enables us to save and get the most recent score
-const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
 // user can add username to keep their score
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     // disables the person to adding a username if there is nothing there
     saveScoreBtn.disabled = !username.value;
 })
@@ -16,4 +21,23 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+    // presenting score and highpoints together
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    
+    // if b score is higher than a score, put b before a - sorting array
+    highScores.sort ((a,b) => b.score - a.score);
+
+    // set a max amount of high scores, only top 5 -- using splice
+    highScores.splice(5);
+
+    // update the high score
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    
+    // go back home
+    window.location.assign("index.html");
 };
